88ffdc806d7ab8af01ab714346c7fea85ae23ed3
tyholland <tyronehus@gmail.com>
2023-02-02 09:05:34 -0600

    add league creator field

--- controllers/leagueControllers/league.controller.js
+++ controllers/leagueControllers/league.controller.js
@@ -8,7 +8,7 @@ module.exports.getLeague = async (req, res) => {
 
   try {
     const leagueData = await mysql(
-      'SELECT l.name, l.num_teams, t.id as teamId FROM league l, league_members lm, team t WHERE lm.league_id = ? AND lm.user_id = ? AND lm.id = t.league_member_id',
+      'SELECT l.name, l.num_teams, l.creator_id, t.id as teamId FROM league l, league_members lm, team t WHERE lm.league_id = ? AND lm.user_id = ? AND lm.id = t.league_member_id',
       [league_id, userId]
     );
 
