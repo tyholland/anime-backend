a87ef132bdb904c29290d5fd0dcecb4a87929d0d
tyholland <tyronehus@gmail.com>
2023-02-11 23:13:34 -0600

    refactor mysql connection

--- utils/mysql.js
+++ utils/mysql.js
@@ -3,14 +3,22 @@ const util = require('util');
 let connection;
 
 const connectToDB = () => {
-  connection = mysql.createPool({
-    host: process.env.REACT_APP_MYSQL_HOST,
+  const config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: process.env.REACT_APP_MYSQL_PORT,
-  });
+  };
 
+  if (process.env.REACT_APP_SERVER_ENV === 'prod') {
+    config.socketPath = process.env.INSTANCE_UNIX_SOCKET;
+  } else {
+    config.host = process.env.REACT_APP_MYSQL_HOST;
+    config.port = process.env.REACT_APP_MYSQL_PORT;
+  }
+
+  connection = mysql.createConnection(config);
+
+  connection.connect();
   return util.promisify(connection.query).bind(connection);
 };
 
