fc9d04950155624d12b74593424efb0d54eee56d
tyholland <tyronehus@gmail.com>
2023-03-27 21:59:42 -0500

    remove cache from matchup page

--- routes/matchupRoutes/matchup.route.js
+++ routes/matchupRoutes/matchup.route.js
@@ -7,22 +7,10 @@ const {
   addVotes,
 } = require('../../controllers/matchupControllers/matchup.controller');
 const { authenticateToken } = require('../../utils');
-const { cacheThirtyMins } = require('../../utils/cache');
-const cache = require('../../utils/cache').instance();
 
 module.exports = (app) => {
-  app.get(
-    '/matchup/:matchup_id',
-    cache(cacheThirtyMins),
-    authenticateToken,
-    getMatchup
-  );
-  app.get(
-    '/matchup/team/:team_id',
-    cache(cacheThirtyMins),
-    authenticateToken,
-    getMatchupFromTeam
-  );
+  app.get('/matchup/:matchup_id', authenticateToken, getMatchup);
+  app.get('/matchup/team/:team_id', authenticateToken, getMatchupFromTeam);
   app.post('/matchup/vote/:matchup_id', authenticateToken, createMatchupVotes);
   app.get('/matchup/votes/:vote_id', getMatchupVotes);
   app.put('/matchup/all/votes', getAllMatchupVotes);
